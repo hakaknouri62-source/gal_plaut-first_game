00b0c96d09da2fe3a229fe38846ae50201039db5
gililililili <96991148+gililililili@users.noreply.github.com>
2026-03-29 23:32:29 +0300

    added rip my granny she got hit by a bazooka(explosion upon strong collision) along with effects

--- galgel ota/Assets/MusicOnCollide.cs
+++ galgel ota/Assets/MusicOnCollide.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class MusicOnCollide : MonoBehaviour
 {
-    public AudioSource audioSource;
+    private AudioSource audioSource;
+    public GameObject Explosion;
+    public float boomScale;
+    public AudioSource boomSong;
+    public float boomThreshold;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Start() 
     {
+        audioSource = GetComponent<AudioSource>();
         // collidor = GetComponent<CircleCollidor2D>(); 
-        // public audioSource = GetComponent<AudioSource>();
     }
 
     // Called on every collision
@@ -19,6 +23,44 @@ public class MusicOnCollide : MonoBehaviour
             // Plays the sound
             audioSource.Play();
         }
+
+        //if we hit it hard enough it also explodess
+        float impactSpeed = collision.relativeVelocity.magnitude;
+
+        if (impactSpeed >= boomThreshold)
+        {
+            // Calculate scale based on speed
+            float size = impactSpeed * boomScale;
+            Explode(size);
+        }
+        
+    }
+    void Explode(float boomSize)
+    {
+// 1. Spawn Visuals
+        GameObject boom = Instantiate(Explosion, transform.position, Quaternion.identity);
+        boom.transform.localScale *= boomSize;
+        // 2. Physical Blast
+        Collider2D[] objectsInRange = Physics2D.OverlapCircleAll(transform.position, boomSize);
+        
+        foreach (Collider2D col in objectsInRange)
+        {
+            Rigidbody2D rb = col.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                // Calculate direction from explosion to object
+                Vector2 direction = (col.transform.position - transform.position).normalized;
+                rb.AddForce(direction * boomSize);
+            }
+        }
+
+        // 3. RIP my granny she got hit by a bazooka
+        boomSong.Play();
+
+        // 4. Remove the original ball
+        Destroy(gameObject);
+        Destroy(boom,1);
+    
     }
     // Update is called once per frame
     void Update()
